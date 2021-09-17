1502a39339f92eaccd94e363a2808116f169e4d8
Lutz Jaenicke <ljaenicke@phoenixcontact.com>
2021-09-17 18:43:20 +0200

    Work forward with Archive Time Stamp

--- crypto/cms/cms_cades.c
+++ crypto/cms/cms_cades.c
@@ -144,3 +144,121 @@ err:
     TS_VERIFY_CTX_free(verify_ctx);
     return ret;
 }
+
+/* The Archive Timestamp Token comes inside a (unsigned) X509 attribute of SignerInfo. */
+/* The token is in PKCS7 format and needs to be converted from its still encoded form */
+int ossl_cms_handle_CAdES_ArchiveTimestampV3Token(X509_ATTRIBUTE *tsattr, X509_STORE *store) {
+    int ret = 0, f = 0;
+    TS_VERIFY_CTX *verify_ctx= NULL;
+    ASN1_TYPE *type = X509_ATTRIBUTE_get0_type(tsattr, 0);
+    int tag = ASN1_TYPE_get(type);
+    ASN1_OCTET_STRING *str = X509_ATTRIBUTE_get0_data(tsattr, 0, tag, NULL);
+    PKCS7 *token = ASN1_item_unpack(str, ASN1_ITEM_rptr(PKCS7));
+    CMS_ContentInfo *cms = ASN1_item_unpack(str, ASN1_ITEM_rptr(CMS_ContentInfo));
+    unsigned char *imprint;
+    unsigned int imprint_len = 0;
+
+    if (!token) {
+        fprintf(stderr, "Could not extract token\n");
+	ERR_print_errors_fp(stderr);
+	goto err;
+    }
+    if (!cms) {
+        fprintf(stderr, "Could not extract CMS\n");
+	ERR_print_errors_fp(stderr);
+	goto err;
+    }
+
+    /*
+     * The archive timestamp token is of coursed signed with a digital signature
+     * using a signerInfo structure with signed and unsigned attributes. Need to extract
+     * the additional hash information from the unsigned attributes.
+     * Note: Since this relies on CAdES background this work is done on CMS structures
+     * instead of PKCS7 structures.
+     */
+    {
+    int i, j, num;
+    CMS_SignerInfo *si;
+    STACK_OF(CMS_SignerInfo) *sinfos;
+    CMS_ATSHashIndexV3 *hashindex;
+#if 0
+    sinfos = (STACK_OF(CMS_SignerInfo) *)PKCS7_get_signer_info(token);
+#else
+    sinfos = CMS_get0_SignerInfos(cms);
+#endif
+    if (!sinfos || !sk_CMS_SignerInfo_num(sinfos)) {
+        ERR_raise(ERR_LIB_PKCS7, PKCS7_R_NO_SIGNATURES_ON_DATA);
+        goto err;
+    }
+    for (i = 0; i < sk_CMS_SignerInfo_num(sinfos); i++) {
+        si = sk_CMS_SignerInfo_value(sinfos, i);
+        num = CMS_unsigned_get_attr_count(si);
+        fprintf(stderr, "ArchiveTS: found %d unsigned attributes\n", num);
+        if (num < 0)
+            continue;
+        for (j = 0; j < num; j++) {
+            X509_ATTRIBUTE *attr = CMS_unsigned_get_attr(si, j);
+            ASN1_OBJECT *obj = X509_ATTRIBUTE_get0_object(attr);
+            switch (OBJ_obj2nid(obj)) {
+                case (NID_id_aa_ATSHashIndex_v3):
+                    fprintf(stderr, "    ATSHashIndex-v3 found \n");
+                    ASN1_TYPE *hi_type = X509_ATTRIBUTE_get0_type(attr, 0);
+                    int hi_tag = ASN1_TYPE_get(hi_type);
+                    ASN1_OCTET_STRING *hi_str = X509_ATTRIBUTE_get0_data(attr, 0, hi_tag, NULL);
+		    hashindex = ASN1_item_unpack(hi_str, ASN1_ITEM_rptr(CMS_ATSHashIndexV3));
+                    if (hashindex == NULL) {
+                        fprintf(stderr, "    Failed to unpack ATSHashIndex-v3\n");
+                        ERR_print_errors_fp(stderr);
+                        goto err;
+                    }
+                    fprintf(stderr, "    hashindex available\n");
+                    break;
+                default:
+                    ; /* don't care */
+            }
+        }
+    }
+
+    }
+
+    f = TS_VFY_VERSION | TS_VFY_SIGNER;
+
+    verify_ctx = TS_VERIFY_CTX_new();
+    if (verify_ctx == NULL)
+        goto err;
+#if 0
+    if (!ossl_cms_cades_extract_timestamp(token, stamp_time)) {
+        fprintf(stderr, "failed to extact stamping time\n");
+        goto err;
+    }
+
+    f |= TS_VFY_IMPRINT;
+    if (!ossl_cms_cades_compute_imprint(token, os, &imprint, &imprint_len))
+	goto err;
+
+    if (TS_VERIFY_CTX_set_imprint(verify_ctx, imprint, imprint_len) == NULL) {
+        fprintf(stderr, "invalid digest string\n");
+        goto err;
+    }
+
+    TS_VERIFY_CTX_add_flags(verify_ctx, f | TS_VFY_SIGNATURE);
+
+    /* TS_VERIFY_CTX_free() will free the store, so we need to up the refcount here */
+    X509_STORE_up_ref(store);
+    if (TS_VERIFY_CTX_set_store(verify_ctx, store) == NULL) {
+        fprintf(stderr, "cannot set store\n");
+	goto err;
+    };
+
+    ret = TS_RESP_verify_token(verify_ctx, token);
+    if (!ret)
+	ERR_print_errors_fp(stderr);
+#else
+    ret = 1;
+    fprintf(stderr, "In ArchiveTimetampToken\n");
+#endif
+
+err:
+    TS_VERIFY_CTX_free(verify_ctx);
+    return ret;
+}
